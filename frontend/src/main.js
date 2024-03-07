3e8cf0bbc0a6e7cfae0472f10078cffcfe03906a
Adhi Satria <adhi.satria@mekari.com>
2024-03-07 22:28:54 +0700

    feat(tic-tac-toe): add vuex package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const store = createStore({
+  state() {
+    return {
+      currentPlayer: 'X'
+    }
+  },
+  mutations: {
+    changePlayer (state) {
+      if (state.currentPlayer == "X") {
+        state.currentPlayer = "O"
+      }else{
+        state.currentPlayer = "X"
+      }
+    }
+  }
+})
+
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
