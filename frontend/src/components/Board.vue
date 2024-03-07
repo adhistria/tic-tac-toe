2eb57d53296c833b0904be9de1c2792610776f1f
Adhi Satria <adhi.satria@mekari.com>
2024-03-07 20:59:34 +0700

    feat(tic-tac-toe): initialize frontend

---
+++ frontend/src/components/Board.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Square from './Square.vue'
+// import {Row} from boostrap
+</script>
+
+<template>
+  <div class="board-row">
+    <div class="board-row">
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+    </div>
+    <div class="board-row">
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+    </div>
+    <div class="board-row">
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+    </div>
+  </div>
+  
+</template>
+
+<style scoped>  
+  .board-row:after {
+    clear: both;
+    content: '';
+    display: table;
+  }
+
+</style>
